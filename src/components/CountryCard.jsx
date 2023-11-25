a6d990603b62dec515af6fca77070108bb50e3cc
SanketPotdar121695 <sanket.potdar563@gmail.com>
2023-11-25 22:49:10 +0530

    country cards component done, taken care of broken images

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Styles from '../styles/CountryCard.module.css';
 
-const CountryCard = ({ id }) => {
+const CountryCard = ({ altSpellings, name }) => {
+  const fallbackSrc = 'https://i.postimg.cc/pdCBrWD9/no-img.png';
+  const baseSrc = `https://flagsapi.com/${altSpellings[0]}/shiny/64.png`;
+
   return (
-    <div>
-      <img src='' alt='' />
-      <h3>hello {id}</h3>
+    <div className={Styles.container}>
+      <img
+        src={baseSrc}
+        onError={(e) => (e.target.src = fallbackSrc)}
+        alt={name.common}
+        width='64px'
+        height='64px'
+      />
+
+      <h3>{name.common}</h3>
     </div>
   );
 };
