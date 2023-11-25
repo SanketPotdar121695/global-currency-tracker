beb90e0bac5a5ae3fffb284b3c1773a44bf5d4a2
SanketPotdar121695 <sanket.potdar563@gmail.com>
2023-11-25 22:47:37 +0530

    set up pagination components for stylings

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Styles from '../styles/Pagination.module.css';
 
 const Pagination = ({
   perPage,
@@ -9,25 +10,24 @@ const Pagination = ({
   const totalPages = Math.ceil(countriesLength / perPage);
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '10px'
-      }}
-    >
+    <div className={Styles.container}>
       {activePage !== 1 && (
         <button onClick={() => handlePageChange(activePage - 1)}>Prev</button>
       )}
 
       {[...Array(totalPages)].map((item, index) => (
-        <button key={index} onClick={() => handlePageChange(index + 1)}>
+        <button
+          className={activePage === index + 1 ? Styles.activePage : ''}
+          key={index}
+          onClick={() => handlePageChange(index + 1)}
+        >
           {index + 1}
         </button>
       ))}
 
-      {activePage !== totalPages && <button>Next</button>}
+      {activePage !== totalPages && (
+        <button onClick={() => handlePageChange(activePage + 1)}>Next</button>
+      )}
     </div>
   );
 };
